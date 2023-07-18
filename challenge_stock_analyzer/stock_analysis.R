a4ef2a3d8c3b9c260f1711de6e395e9871d30daa
cgx5813 <paul.roth@tuhh.de>
2023-07-18 15:19:40 +0200

    Zwischenstand

---
+++ challenge_stock_analyzer/stock_analysis.R
@@ -0,0 +1,206 @@
+# Business Analytics with Data Science and Machine Learning ----
+# Building Business Data Products ----
+# STOCK ANALYZER APP - DATA ANALYSIS -----
+
+# APPLICATION DESCRIPTION ----
+# - The user will select 1 stock from the SP 500 stock index
+# - The functionality is designed to pull the past 180 days of stock data by default
+# - We will implement 2 moving averages - short (fast) and long (slow)
+# - We will produce a timeseries visualization
+# - We will produce automated commentary based on the moving averages
+
+# LIBRARIES ----
+library(tidyverse)
+library(fs)
+library(glue)
+
+library(rvest)
+library(quantmod)
+
+library(plotly)
+
+# 1.0 GET STOCK LIST ----
+
+get_stock_list <- function(stock_index = "DAX") {
+  
+  # Control for upper and lower case
+  index_lower <- str_to_lower(stock_index)
+  # Control if user input is valid
+  index_valid <- c("dax", "sp500", "dow", "nasdaq")
+  if (!index_lower %in% index_valid) {stop(paste0("x must be a character string in the form of a valid exchange.",
+                                                  " The following are valid options:\n",
+                                                  stringr::str_c(str_to_upper(index_valid), collapse = ", ")))
+  }
+  
+  # Control for different currencies and different column namings in wiki
+  vars <- switch(index_lower,
+                 dax    = list(wiki     = "DAX", 
+                               columns  = c("Ticker symbol", "Company")),
+                 sp500  = list(wiki     = "List_of_S%26P_500_companies", 
+                               columns  = c("Symbol", "Security")),
+                 dow    = list(wiki     = "Dow_Jones_Industrial_Average",
+                               columns  = c("Symbol", "Company")),
+                 nasdaq = list(wiki     = "NASDAQ-100",
+                               columns  = c("Ticker", "Company"))
+  )
+  
+  # Extract stock list depending on user input
+  read_html(glue("https://en.wikipedia.org/wiki/{vars$wiki}")) %>% 
+    
+    # Extract table from wiki
+    html_nodes(css = "#constituents") %>% 
+    html_table() %>% 
+    dplyr::first() %>% 
+    as_tibble(.name_repair = "minimal") %>% 
+    # Select desired columns (different for each article)
+    dplyr::select(vars$columns) %>% 
+    # Make naming identical
+    set_names(c("symbol", "company")) %>% 
+    
+    # Clean (just relevant for DOW)
+    mutate(symbol = str_remove(symbol, "NYSE\\:[[:space:]]")) %>% 
+    
+    # Sort
+    arrange(symbol) %>%
+    # Create the label for the dropdown list (Symbol + company name)
+    mutate(label = str_c(symbol, company, sep = ", ")) %>%
+    dplyr::select(label)
+  
+}
+
+
+# 2.0 EXTRACT SYMBOL BASED ON USER INPUT ----
+
+library(stringr)
+library(purrr)
+
+user_input <- "AAPL, Apple Inc."
+
+split_string <- str_split(user_input, ",\\s*")[[1]]
+ticker <- split_string[1]
+
+
+get_symbol_from_user_input <- function(user_input) {
+  split_string <- str_split(user_input, ",\\s*")[[1]]
+  ticker <- split_string[1]
+}
+
+library(quantmod)
+library(timetk)
+library(dplyr)
+library(stringr)
+library(lubridate)
+library(zoo)
+
+get_stock_data <- function(stock_symbol, from, to, mavg_short = 20, mavg_long = 50) {
+  stock_data <- quantmod::getSymbols(
+    stock_symbol,
+    src = "yahoo",
+    from = from,
+    to = to,
+    auto.assign = FALSE
+  ) %>%
+    timetk::tk_tbl(preserve_index = TRUE, silent = TRUE) %>%
+    mutate(currency = case_when(
+      str_detect(stock_symbol, "\\.DE") ~ "EUR",
+      TRUE ~ "USD"
+    )) %>%
+    na.omit() %>%
+    mutate(mavg_short = zoo::rollmeanr(.data[[colnames(.)[2]]], k = mavg_short, fill = NA),
+           mavg_long = zoo::rollmeanr(.data[[colnames(.)[2]]], k = mavg_long, fill = NA))
+  
+  stock_data_tbl <- stock_data %>%
+    select(date = index, adjusted = .data[[colnames(.)[2]]], mavg_short, mavg_long, currency)
+  
+  return(stock_data_tbl)
+}
+
+# Example usage:
+stock_data_tbl <- get_stock_data("AAPL", from = "2020-06-01", to = "2021-01-12", mavg_short = 5, mavg_long = 50)
+
+
+
+
+
+
+# Example usage:
+stock_data_tbl <- get_stock_data("AAPL", from = "2020-06-01", to = "2021-01-12", mavg_short = 5, mavg_long = 50)
+
+# 3.0 GET STOCK DATA ----
+
+
+
+# 4.0 PLOT STOCK DATA ----
+
+# Add the currency_format function to your script
+currency_format <- function(currency) {
+  if (currency == "USD") {
+    scales::dollar_format(largest_with_cents = 10)
+  } else if (currency == "EUR") {
+    scales::dollar_format(
+      prefix = "",
+      suffix = " €",
+      big.mark = ".",
+      decimal.mark = ",",
+      largest_with_cents = 10
+    )
+  } else {
+    identity  # Default to no formatting if currency is not recognized
+  }
+}
+
+# Modify your plot code
+g <- stock_data_tbl %>%
+  pivot_longer(
+    cols = c(adjusted, mavg_short, mavg_long),
+    names_to = "legend",
+    values_to = "value",
+    names_ptypes = list(legend = factor())
+  ) %>%
+  ggplot(aes(x = date, y = value, color = legend, linetype = legend, group = legend)) +
+  geom_line() +
+  labs(y = "Adjusted Share Price", x = "") +
+  scale_y_continuous(
+    labels = currency_format(stock_data_tbl$currency[[1]])
+  )
+
+# Convert the plot to an interactive plotly plot
+ggplotly(g)
+
+
+
+
+plot_stock_data <- function(stock_data) {
+  g <- stock_data %>%
+    pivot_longer(
+      cols = c(adjusted, mavg_short, mavg_long),
+      names_to = "legend",
+      values_to = "value",
+      names_ptypes = list(legend = factor())
+    ) %>%
+    ggplot(aes(x = date, y = value, color = legend, linetype = legend, group = legend)) +
+    geom_line() +
+    labs(y = "Adjusted Share Price", x = "") +
+    scale_y_continuous(
+      labels = currency_format(stock_data$currency[[1]])
+    )
+  
+  ggplotly(g)
+}
+
+# Example function call
+"ADS.DE" %>% 
+  get_stock_data(from = "2020-06-01", to = "2021-01-12", mavg_short = 5, mavg_long = 50) %>%
+  plot_stock_data()
+
+
+# 5.0 GENERATE COMMENTARY ----
+
+
+
+# 6.0 TEST WORKFLOW ----
+
+
+
+# 7.0 SAVE SCRIPTS ----
+
